78213f6c356d09de8fe9319babb14e65d33640ce
Gautier Ben Aïm <benaimg@bde.enseeiht.fr>
2020-03-27 11:31:31 +0100

    chore: setting up libgdx

---
+++ android/src/com/knightwars/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.knightwars;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.knightwars.KnightWars;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KnightWars(), config);
+	}
+}
