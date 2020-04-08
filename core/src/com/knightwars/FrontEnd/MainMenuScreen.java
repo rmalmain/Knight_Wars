0e0eaf6a87a16f39392fea6ae1e7e5b957592881
Ozon0 <tom.pillot00@gmail.com>
2020-04-08 23:53:46 +0200

    feat: add menu and game screens

---
+++ core/src/com/knightwars/FrontEnd/MainMenuScreen.java
@@ -0,0 +1,69 @@
+package com.knightwars.FrontEnd;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.GL20;
+
+import com.knightwars.KnightWars;
+
+public class MainMenuScreen implements Screen {
+
+    private final KnightWars game;
+    private OrthographicCamera camera;
+
+    public MainMenuScreen(final KnightWars game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 500, 500);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to KnightWars! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
