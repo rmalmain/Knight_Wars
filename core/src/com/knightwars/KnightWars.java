123565fc01831094c7a0a7a9b3148bd6c9234763
Ozon0 <tom.pillot00@gmail.com>
2020-04-08 23:53:46 +0200

    feat: add menu and game screens

--- core/src/com/knightwars/KnightWars.java
+++ core/src/com/knightwars/KnightWars.java
@@ -1,33 +1,33 @@
 package com.knightwars;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.knightwars.FrontEnd.*;
 
-public class KnightWars extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class KnightWars extends Game {
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+	private GameScreen front;
 	
 	@Override
 	public void create () {
+		front = new GameScreen(this);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont(); //Use LibGDX's default Arial font.
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		front.dispose();
 	}
 }
