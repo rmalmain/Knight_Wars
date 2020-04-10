af5f6f3e44b7f7311fd88e7750a474a5a2b87c5b
rmalmain <rmalmain@pm.me>
2020-04-10 12:14:08 +0200

    feat: Added GameManager.java

---
+++ core/src/com/knightwars/game/GameManager.java
@@ -0,0 +1,13 @@
+package com.knightwars.game;
+
+public class GameManager {
+    private Game game;
+
+    GameManager() { this.game = new Game(); }
+
+    public void update(int dt) {}
+}
+
+/*
+init avec un map, nb de joueurs..
+interaction entre front et back */
