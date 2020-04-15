0c8a60ca774f195cf83f14cc5ddd8d774ea917dc
rmalmain <rmalmain@pm.me>
2020-04-15 10:45:51 +0200

    feat: add Building.java feat: first version

---
+++ core/src/com/knightwars/game/environment/Building.java
@@ -0,0 +1,19 @@
+package com.knightwars.game.environment;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Building {
+    public Player owner;
+    public Vector2 coordinates;
+
+    public Building(Player owner, Vector2 coordinates) {
+        this.owner = owner;
+        this.coordinates = new Vector2(coordinates);
+    }
+
+    /* Owner getter */
+    public Player getOwner() { return this.owner; }
+
+    /* Coordinates getter */
+    public Vector2 getCoordinates() { return new Vector2(this.coordinates);}
+}
