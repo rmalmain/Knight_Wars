2f57cb66f22a724d32016b9ff36080efe7383077
rmalmain <rmalmain@pm.me>
2020-04-15 11:52:16 +0200

    feat: Initial commit

--- core/src/com/knightwars/game/environment/Map.java
+++ core/src/com/knightwars/game/environment/Map.java
@@ -1,4 +1,20 @@
 package com.knightwars.game.environment;
 
+import com.badlogic.gdx.math.Vector2;
+import java.util.ArrayList;
+
 public class Map {
+    public ArrayList<Building> buildings;
+    public Vector2 size;
+
+    public Map(float width, float height) {
+        this.size = new Vector2(width, height);
+        this.buildings = new ArrayList<>();
+    }
+
+    public void addBuilding(Building building) {
+        buildings.add(new Building(building));
+    }
+
+    public ArrayList<Building> getBuildings() { return buildings; }
 }
