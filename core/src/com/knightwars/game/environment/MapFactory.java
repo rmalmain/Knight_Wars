8b0448d75f82a8eb7e1d054e641618ed198251e2
rmalmain <rmalmain@pm.me>
2020-04-15 13:17:42 +0200

    feat: greatly improved generation with implementation of a threshold

--- core/src/com/knightwars/game/environment/MapFactory.java
+++ core/src/com/knightwars/game/environment/MapFactory.java
@@ -17,11 +17,25 @@ public class MapFactory {
 
         float x, y;
         for (int i = 0; i < buildings; i++) {
-            x = ((float) Math.random() * 0.9f + 0.05f) * width;
-            y = ((float) Math.random() * 0.9f + 0.05f) * height;
+            do {
+                x = ((float) Math.random() * 0.9f + 0.05f) * width;
+                y = ((float) Math.random() * 0.9f + 0.05f) * height;
+            } while (!isValidPoint(proceduralMap, new Vector2(x, y), 0.4f)); // generating points until it is
+                                                                                // not too close to other buildings
+
             proceduralMap.addBuilding(new Building(defaultPlayer, new Vector2(x, y)));
         }
         return proceduralMap;
+    }
+
+    private static Boolean isValidPoint(Map map, Vector2 point, float threshold) {
+        for (int i=0; i<map.getBuildings().size(); i++) {
+            if (point.dst(map.getBuildings().get(i).coordinates) < threshold) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
