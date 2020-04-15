17e540566ef152e3a326a3b13c517051dcc3145b
Ozon0 <tom.pillot00@gmail.com>
2020-04-15 22:07:04 +0200

    refactor: split GameScreen into multiple actors

--- core/src/com/knightwars/userInterface/GameScreen.java
+++ core/src/com/knightwars/userInterface/GameScreen.java
@@ -1,32 +1,29 @@
+/*
+ * Draw the game
+ */
+
+
 package com.knightwars.userInterface;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.knightwars.game.KnightWarsGame;
-import com.knightwars.game.environment.Building;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class GameScreen implements Screen {
 
     private final Display display;
-    private final KnightWarsGame gameState;
-
+    private final Stage stage;
     private final FitViewport viewport;
     private final SpriteBatch batch;
-    private final Sprite spriteRedUnit;
-    private final Sprite spriteRedBuilding;
-    private final Sprite spriteBlueBuilding;
-    private final Sprite spriteNeutralBuilding;
     private final Camera camera;
     private final Vector2 mapSize;
 
@@ -34,22 +31,21 @@ public class GameScreen implements Screen {
 
     public GameScreen(final Display display, KnightWarsGame gameState) {
         this.display = display;
-        this.gameState = gameState;
         batch = new SpriteBatch();
-        spriteRedUnit = new Sprite(new Texture("red_unit.png"));
-        spriteRedBuilding = new Sprite(new Texture("red_building.png"));
-        spriteBlueBuilding = new Sprite(new Texture("blue_building.png"));
-        spriteNeutralBuilding = new Sprite(new Texture("neutral_building.png"));
 
         // Fetch the map size from the game state
-        mapSize = gameState.getMap().getSize(); //new Vector2(3f, 2f);
+        mapSize = gameState.getMap().getSize();
 
-        // Constructs a new OrthographicCamera, using the given viewport width and height
-        // Height is multiplied by aspect ratio.
+        // Create a new camera
         camera = new OrthographicCamera();
-        camera.position.set(mapSize.x * SCALE / 2, mapSize.y * SCALE / 2, 0);
 
+        // Create a viewport so the proportions of the displayed map are always preserved
         viewport = new FitViewport(mapSize.x * SCALE, mapSize.y * SCALE, camera);
+
+        // Create a stage and add all the actors to display
+        stage = new Stage(viewport, batch);
+        stage.addActor(new GameScreenMapDisplay(gameState));
+        stage.addActor(new GameScreenHUD(gameState));
     }
 
 
@@ -68,21 +64,9 @@ public class GameScreen implements Screen {
      */
     @Override
     public void render(float delta) {
-        // Fetch the coordinates of the buildings and units
-        ArrayList<Building> buildings = gameState.getMap().getBuildings();
-        List<Vector2> unitCoordinates = new ArrayList<>();
-        unitCoordinates.add(new Vector2(1f, 1f));
-
         // Clear last screen
         Gdx.gl.glClearColor(0, 0.3f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Update the camera and the inverse window projection
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-        Vector3 pos = new Vector3();
-        pos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-        viewport.unproject(pos);
 
         // Draw the map boundaries
         ShapeRenderer shapeRenderer = new ShapeRenderer();
@@ -92,33 +76,8 @@ public class GameScreen implements Screen {
         shapeRenderer.rect(0, 0, mapSize.x * SCALE, mapSize.y * SCALE);
         shapeRenderer.end();
 
-        // Draw the map elements
-        batch.begin();
-        for (Building building : buildings) {
-            Vector2 buildingCoordinates = building.getCoordinates();
-            batch.draw(determineBuildingSprite(building), buildingCoordinates.x * SCALE - spriteRedBuilding.getWidth() / 2f,
-                    buildingCoordinates.y * SCALE - spriteRedUnit.getHeight() / 2f);
-        }
-        for (Vector2 unitCoordinate : unitCoordinates) {
-            batch.draw(spriteRedUnit, unitCoordinate.x * SCALE - spriteRedBuilding.getWidth() / 2f,
-                    unitCoordinate.y * SCALE - spriteRedBuilding.getHeight() / 2f);
-        }
-        batch.end();
-    }
-
-    private Sprite determineBuildingSprite(Building building) {
-        if (building.getOwner() == gameState.getPlayerRed()) {
-            return spriteRedBuilding;
-        }
-        else if (building.getOwner() == gameState.getPlayerBlue()) {
-            return spriteBlueBuilding;
-        }
-        else if (building.getOwner() == gameState.getPlayerNeutral()) {
-            return spriteNeutralBuilding;
-        }
-        else {
-            throw new UnknownPlayerException("There is no player associated with the building.");
-        }
+        stage.act();
+        stage.draw();
     }
 
     /**
