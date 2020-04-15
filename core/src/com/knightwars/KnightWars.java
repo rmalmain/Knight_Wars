da4e2fcab92eae6182507de5d70877e115cb69c9
romuche98 <romain.malmain6@gmail.com>
2020-04-15 10:48:09 +0200

    chore: reorganization

--- core/src/com/knightwars/KnightWars.java
+++ core/src/com/knightwars/KnightWars.java
@@ -1,18 +1,21 @@
 package com.knightwars;
 
 import com.badlogic.gdx.Game;
+import com.knightwars.game.GameManager;
 import com.knightwars.game.KnightWarsGame;
 import com.knightwars.userInterface.Display;
 
 public class KnightWars extends Game {
 
-	public KnightWarsGame gameState;
+	private GameManager gameManager;
+	private KnightWarsGame game;
 
 	@Override
 	public void create () {
-		gameState = new KnightWarsGame();
+		gameManager = new GameManager();
+		game = gameManager.getKnightWarsGame();
 
-		Display display = new Display(this, gameState);
+		Display display = new Display(this, game);
 		display.displayMainMenu();
 	}
 
