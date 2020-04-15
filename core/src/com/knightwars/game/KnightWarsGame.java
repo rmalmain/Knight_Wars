8346f5d6fd61a5a0abafcb4c81e56218eff91699
rmalmain <rmalmain@pm.me>
2020-04-15 12:16:56 +0200

    BREAKING CHANGE: initialize map refactoring

--- core/src/com/knightwars/game/KnightWarsGame.java
+++ core/src/com/knightwars/game/KnightWarsGame.java
@@ -6,18 +6,18 @@ import java.util.HashMap;
 
 public class KnightWarsGame {
 
-    Player playerRed, playerBlue, playerNeutral;
-    private final HashMap<String, Object> map;
+    private Player playerRed, playerBlue, playerNeutral;
+    private Map map;
 
     public KnightWarsGame() {
         this.playerRed = new Player("Red Player", Player.ColorPlayer.RED);
         this.playerBlue = new Player("Blue Player", Player.ColorPlayer.BLUE);
         this.playerNeutral = new Player("Neutral Player", Player.ColorPlayer.NEUTRAL);
 
-        this.map = MapFactory.createMap(3f, 2f, 10);
+        this.map = MapFactory.createProceduralMap(3f, 2f, 10, this.playerNeutral);
     }
 
-    public HashMap<String, Object> getMap() {
+    public Map getMap() {
         return map;
     }
 
