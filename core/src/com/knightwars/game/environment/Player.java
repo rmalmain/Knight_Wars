72d6a7c66dc0e986dd36a89f32ce8e0fb4ac6e77
romuche98 <romain.malmain6@gmail.com>
2020-04-15 11:05:17 +0200

    feat: initial commit

--- core/src/com/knightwars/game/environment/Player.java
+++ core/src/com/knightwars/game/environment/Player.java
@@ -1,4 +1,26 @@
 package com.knightwars.game.environment;
 
 public class Player {
+
+    public enum ColorPlayer {NEUTRAL, BLUE, RED}
+
+    private int gold;
+    private String name;
+    private ColorPlayer color;
+
+    public Player(String name, ColorPlayer color) {
+        this.name = name;
+        this.color = color;
+
+        this.gold = 50; // Default golds, to modify if necessary
+    }
+
+    /* Name getter */
+    public String getName() { return this.name; }
+
+    /* Color getter */
+    public ColorPlayer getColor() { return this.color; }
+
+    /* Gold getter */
+    public int getGold() { return this.gold; }
 }
