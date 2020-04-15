dbbc95d70fa26b6be790b92fc291a3683d883a0f
Ozon0 <tom.pillot00@gmail.com>
2020-04-15 18:29:36 +0200

    feat: changed default window size

--- desktop/src/com/knightwars/desktop/DesktopLauncher.java
+++ desktop/src/com/knightwars/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.knightwars.KnightWars;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 640;
+		config.width = 960;
 		config.forceExit = false; // to prevent non-zero exit value when the window is closed
 		new LwjglApplication(new KnightWars(), config);
 	}
