eab0fe854dceb84591893f364e2511f783bbf936
romuche98 <romain.malmain6@gmail.com>
2020-04-15 14:24:26 +0200

    feat: map contains units

--- core/src/com/knightwars/game/environment/Map.java
+++ core/src/com/knightwars/game/environment/Map.java
@@ -5,20 +5,26 @@ import java.util.ArrayList;
 
 public class Map {
     public ArrayList<Building> buildings;
+    public ArrayList<Unit> units;
     public Vector2 size;
 
     public Map(float width, float height) {
         this.size = new Vector2(width, height);
         this.buildings = new ArrayList<>();
+        this.units = new ArrayList<>();
     }
 
     public void addBuilding(Building building) {
         buildings.add(new Building(building));
     }
 
-    public Vector2 getSize() {
-        return new Vector2(size);
-    }
+    public void addUnit(Unit unit) { units.add(unit); }
+
+    public void deleteUnit(Unit unit) { units.remove(unit); }
+
+    public Vector2 getSize() { return new Vector2(size); }
 
     public ArrayList<Building> getBuildings() { return buildings; }
+
+    public ArrayList<Unit> getUnits() { return units; }
 }
