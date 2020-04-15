e648840275fdeda1fada63db582e24236c5c2cde
rmalmain <rmalmain@pm.me>
2020-04-15 23:51:42 +0200

    docs: added some docs, little code refactoring

--- core/src/com/knightwars/game/KnightWarsGame.java
+++ core/src/com/knightwars/game/KnightWarsGame.java
@@ -7,23 +7,30 @@ import com.knightwars.game.environment.Player;
 import static com.badlogic.gdx.math.MathUtils.random;
 
 public class KnightWarsGame {
+    public static final float WIDTH = 6f;
+    public static final float HEIGHT = 4f;
+    public static final int BUILDINGS_NUMBER = 10;
+    public static final float BUILDING_GENERATION_THRESHOLD = 0.9f;
+    public static final float BUILDING_COLLISION_THRESHOLD = 0.1f;
 
     private Player playerRed, playerBlue, playerNeutral;
     private Map map;
 
     public KnightWarsGame() {
+        // Players initialization
         this.playerRed = new Player("Red Player", Player.ColorPlayer.RED);
         this.playerBlue = new Player("Blue Player", Player.ColorPlayer.BLUE);
         this.playerNeutral = new Player("Neutral Player", Player.ColorPlayer.NEUTRAL);
 
-        this.map = MapFactory.createProceduralMap(6f, 4f, 10, this.playerNeutral, 0.4f);
+        this.map = MapFactory.createProceduralMap( // Map generation
+                WIDTH, HEIGHT, BUILDINGS_NUMBER, this.playerNeutral,
+                BUILDING_GENERATION_THRESHOLD, BUILDING_COLLISION_THRESHOLD);
 
+        // Buildings attribution
         attributeBuildings();
     }
 
-    /* Attribute a building to each player.
-    *  Every other buildings are attributed to the neutral player.
-    */
+    /** Attribute a building to each player. Every other buildings are attributed to the neutral player. */
     public void attributeBuildings() {
         int randomRed = random(map.getBuildings().size()-1);
         int randomBlue = random(map.getBuildings().size()-1);
@@ -41,7 +48,5 @@ public class KnightWarsGame {
 
     public Player getPlayerNeutral() { return this.playerNeutral; }
 
-    public Map getMap() {
-        return map;
-    }
+    public Map getMap() { return map; }
 }
