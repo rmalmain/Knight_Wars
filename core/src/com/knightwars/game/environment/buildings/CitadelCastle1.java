eaec3276f5fa0fa74b4e892d9bf3eaaaafede0fd
einderjam <clementrivola@gmail.com>
2020-05-22 10:42:44 +0200

    fix: Refactor: Castle names

--- core/src/com/knightwars/game/environment/buildings/CitadelCastle1.java
+++ core/src/com/knightwars/game/environment/buildings/CitadelCastle1.java
@@ -4,16 +4,43 @@ import com.badlogic.gdx.math.Vector2;
 import com.knightwars.game.environment.Building;
 import com.knightwars.game.players.Player;
 
+/**
+ * This is a Garrison Castle : specialized in Knights generation
+ */
 public class CitadelCastle1 extends Building {
+    public final static float BUILDING_RANGE = 1.0f;
+    public final static float ARROW_SPEED = 2.0f;
+    public final static float ARROW_RATE = 1.0f;
+
+    private float timeElapsedSinceLastArrow;
+
     public CitadelCastle1(Player owner, Vector2 coordinates, int knights, boolean knightGrowth) {
         super(owner, coordinates, knights, knightGrowth);
+        this.timeElapsedSinceLastArrow = 0f;
     }
 
+    /** Construct a building from another building.
+     * @param building the building to copy
+     */
     public CitadelCastle1(Building building) {
         this(building.getOwner(), building.getCoordinates(), building.getKnights(), building.getCanGenerateUnits());
     }
 
     public CitadelCastle1 Copy() {
         return new CitadelCastle1(this);
+    }
+
+    private boolean canShoot() {
+        return this.timeElapsedSinceLastArrow > ARROW_RATE;
+    }
+
+    private void shootArrow() {
+        this.timeElapsedSinceLastArrow = 0f;
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+
     }
 }
--- core/src/com/knightwars/game/environment/buildings/GarrisonCastle1.java
+++ core/src/com/knightwars/game/environment/buildings/GarrisonCastle1.java
@@ -5,15 +5,9 @@ import com.knightwars.game.environment.Building;
 import com.knightwars.game.players.Player;
 
 public class GarrisonCastle1 extends FortifiedCastle {
-    public final static float BUILDING_RANGE = 1.0f;
-    public final static float ARROW_SPEED = 2.0f;
-    public final static float ARROW_RATE = 1.0f;
-
-    private float timeElapsedSinceLastArrow;
 
     public GarrisonCastle1(Player owner, Vector2 coordinates, int knights, boolean knightGrowth) {
         super(owner, coordinates, knights, knightGrowth);
-        this.timeElapsedSinceLastArrow = 0f;
     }
 
     /** Construct a building from another building.
@@ -27,17 +21,4 @@ public class GarrisonCastle1 extends FortifiedCastle {
         return new GarrisonCastle1(this);
     }
 
-    private boolean canShoot() {
-        return this.timeElapsedSinceLastArrow > ARROW_RATE;
-    }
-
-    private void shootArrow() {
-        this.timeElapsedSinceLastArrow = 0f;
-    }
-
-    @Override
-    public void update(float dt) {
-        super.update(dt);
-
-    }
 }
