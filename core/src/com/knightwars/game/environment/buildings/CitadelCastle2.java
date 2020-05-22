7614898ad59b55d6af78f01b1adbb14307e9b07c
einderjam <clementrivola@gmail.com>
2020-05-22 10:42:44 +0200

    fix: Refactor: Castle names

--- core/src/com/knightwars/game/environment/buildings/CitadelCastle2.java
+++ core/src/com/knightwars/game/environment/buildings/CitadelCastle2.java
@@ -5,18 +5,11 @@ import com.knightwars.game.environment.Building;
 import com.knightwars.game.players.Player;
 
 /**
- * This is a Garrison Castle : specialized in Knights generation
+ * This is a Citadel : Has a turret and throws arrows
  */
 public class CitadelCastle2 extends CitadelCastle1 {
-    public final static float BUILDING_RANGE = 1.0f;
-    public final static float ARROW_SPEED = 2.0f;
-    public final static float ARROW_RATE = 1.0f;
-
-    private float timeElapsedSinceLastArrow;
-
     public CitadelCastle2(Player owner, Vector2 coordinates, int knights, boolean knightGrowth) {
         super(owner, coordinates, knights, knightGrowth);
-        this.timeElapsedSinceLastArrow = 0f;
     }
 
     /** Construct a building from another building.
--- core/src/com/knightwars/game/environment/buildings/GarrisonCastle2.java
+++ core/src/com/knightwars/game/environment/buildings/GarrisonCastle2.java
@@ -4,17 +4,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.knightwars.game.environment.Building;
 import com.knightwars.game.players.Player;
 
-/**
- * This is a Citadel : Has a turret and throws arrows
- */
 public class GarrisonCastle2 extends GarrisonCastle1 {
     public GarrisonCastle2(Player owner, Vector2 coordinates, int knights, boolean knightGrowth) {
         super(owner, coordinates, knights, knightGrowth);
     }
 
-    /** Construct a building from another building.
-     * @param building the building to copy
-     */
     public GarrisonCastle2(Building building) {
         this(building.getOwner(), building.getCoordinates(), building.getKnights(), building.getCanGenerateUnits());
     }
